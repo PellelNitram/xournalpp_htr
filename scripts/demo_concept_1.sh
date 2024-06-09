46e94a2200d5946c6dd5b7d77d6f0484e3a20d26
Martin L <konten.ma.le@gmail.com>
2024-06-09 14:43:59 +0100

    Update demo_concept_1 bash script.

--- scripts/demo_concept_1.sh
+++ scripts/demo_concept_1.sh
@@ -1,4 +1,4 @@
 FILE=~/data/xournalpp_htr/datasets/tests/test_1.xoj
 FILE=../tests/test_1.xoj
 
-python demo_concept_1.py --input-file ${FILE}
+python ../xournalpp_htr/demo_concept_1.py --input-file ${FILE}
