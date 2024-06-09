9a9bb41c089a7f7badf315afd662b79aa130ef42
Martin L <konten.ma.le@gmail.com>
2024-06-09 15:40:43 +0100

    Add utils.export_to_pdf_with_xournalpp function boilerplate.

---
+++ xournalpp_htr/utils.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+
+def export_to_pdf_with_xournalpp(input_file: Path, output_file: Path) -> None:
+    """Execute Xournal(++) file to PDF using Xournal++.
+    
+    TODO
+    """
+    # TODO: Needs to raise Exception if export fails
+    pass
