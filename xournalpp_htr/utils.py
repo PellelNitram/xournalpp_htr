f10eafb2656b22a6b81156d82cdc6c8ccbe1d0cc
Martin L <konten.ma.le@gmail.com>
2024-06-14 13:52:58 +0100

    Work on export_to_pdf_with_xournalpp function.

--- xournalpp_htr/utils.py
+++ xournalpp_htr/utils.py
@@ -1,10 +1,23 @@
 from pathlib import Path
+import subprocess
 
 
 def export_to_pdf_with_xournalpp(input_file: Path, output_file: Path) -> None:
-    """Execute Xournal(++) file to PDF using Xournal++.
+    """Export Xournal(++) file to PDF using Xournal++.
     
     TODO
     """
     # TODO: Needs to raise Exception if export fails
-    raise NotImplementedError('not implemented and also lacking docstring.')
+
+    export_result = subprocess.run(f'xournalpp {input_file} -p {output_file}',
+                               shell=True,
+                               capture_output=True)
+
+    return_code_fail = export_result.returncode != 0
+    stdout_fail = 'PDF file successfully created' not in export_result.stderr.decode('utf-8')
+    file_existing_fail = not output_file.exists()
+
+    if return_code_fail or stdout_fail or file_existing_fail:
+        raise RuntimeError('PDF export failed')
+
+    return output_file
