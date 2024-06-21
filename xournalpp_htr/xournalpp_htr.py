ea5b6917c1c1cba19a9d17d85e4b5bfa980ee12e
Martin L <konten.ma.le@gmail.com>
2024-06-21 17:32:51 +0100

    Work on transferring experiment_with_text_layer notebook code to xournalpp_htr script.

--- xournalpp_htr/xournalpp_htr.py
+++ xournalpp_htr/xournalpp_htr.py
@@ -7,7 +7,8 @@ import cv2
 import matplotlib.pyplot as plt
 from htr_pipeline import read_page, DetectorConfig, LineClusteringConfig
 
-from xournalpp_htr.documents import XournalDocument
+from documents import XournalDocument
+from utils import export_to_pdf_with_xournalpp
 
 
 def parse_arguments():
@@ -15,21 +16,25 @@ def parse_arguments():
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('-if', '--input-file', type=lambda p: Path(p).absolute(), required=True,
                         help='Path to the input Xournal or Xournal++ file.')
+    parser.add_argument('-of', '--output-file', type=lambda p: Path(p).absolute(), required=True,
+                        help='Path to the output PDF file.')
     parser.add_argument('-m', '--model', type=str, required=True,
                         help='The model to use for handwriting recognition.') # TODO: Introduce dummy model called "test_lua_to_python"; TODO: Register models somehow to allow choice keyword here
     args = vars( parser.parse_args() )
     return args
 
 def main(args):
+    """TODO.
     
-    pass
-
-    # TODO: Agenda
-    # 1. export X file to PDF
-    # 2. do HTR on X file using model that is specified by `parse_arguments`
-    # 3. store prediction as hidden text to PDF file
+    TODO: This is what I do here:
+    1. export X file to PDF
+    2. do HTR on X file using model that is specified by `parse_arguments`
+    3. store prediction as hidden text to PDF file
 
     # TODO: Define coordinate transforms between X file, prediction and PDF as matrices like in computer graphics.
+    """
+
+    export_to_pdf_with_xournalpp(args['input_file'], args['output_file'])
 
 if __name__ == '__main__':
     args = parse_arguments()
