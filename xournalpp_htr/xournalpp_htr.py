d0bf89f1c3ba1dd6147b99b1df0efc1be7c08aba
Martin L <konten.ma.le@gmail.com>
2024-06-22 14:11:06 +0100

    Add TODO comment to xournalpp_htr script.

--- xournalpp_htr/xournalpp_htr.py
+++ xournalpp_htr/xournalpp_htr.py
@@ -26,6 +26,7 @@ def parse_arguments():
                         help='Path to the input Xournal or Xournal++ file.')
     parser.add_argument('-of', '--output-file', type=lambda p: Path(p).absolute(), required=True,
                         help='Path to the output PDF file.')
+    # v-- TODO: Make optional
     parser.add_argument('-m', '--model', type=str, required=True,
                         help='The model to use for handwriting recognition.') # TODO: Introduce dummy model called "test_lua_to_python"; TODO: Register models somehow to allow choice keyword here; also add "none"; default to latest model
     parser.add_argument('-pid', '--prediction-image-dir', type=lambda p: Path(p).absolute(), required=False,
