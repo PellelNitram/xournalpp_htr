bb474a5a49efb3f9bcb1f40e4515223aad43c1bd
Martin L <konten.ma.le@gmail.com>
2024-06-22 14:13:04 +0100

    Add bash script to install Lua part of plugin easily.

---
+++ plugin/copy_to_plugin_folder.sh
@@ -0,0 +1,9 @@
+# Potentially needs to be executed using `sudo`
+
+TARGET_FOLDER=~/.config/xournalpp/plugins/xournalpp_htr/
+TARGET_FOLDER=/usr/share/xournalpp/plugins/xournalpp_htr # requires `sudo`
+
+mkdir -p ${TARGET_FOLDER}
+
+cp plugin.ini ${TARGET_FOLDER}
+cp main.lua ${TARGET_FOLDER}
