2c41bdf65ad460351fdafe02be6dd75b53cc4b88
Martin L <konten.ma.le@gmail.com>
2024-06-28 11:03:48 +0100

    Work on Lua plugin.
    Introduce `app.fileDialogSave` and refactor logic into separate function
    that serves as callback of `app.fileDialogSave`.

--- plugin/main.lua
+++ plugin/main.lua
@@ -1,23 +1,31 @@
 function initUi()
   app.registerUi({["menu"] = "Xournal++ HTR", ["callback"] = "run", ["accelerator"] = "<Control>F1"});
 end
-  
+
+function save_file(path)
+  if path:len() > 0 then
+
+    -- Read settings: I use this (https://stackoverflow.com/a/41176958). An
+    -- alternative could have been https://stackoverflow.com/a/41176826. Both
+    -- found using G"lua read settings file".
+    local config = require "config"
+
+    result = app.msgbox("Exports starts now, please wait until finished", {[1] = "Continue", [2] = "Cancel"})
+    if result == 1 then
+      command = config.python_executable .. " " .. config.xournalpp_htr_path
+                .. " -if " .. config.filename
+                .. "  -m " .. config.model
+                .. " -of " .. config.output_file
+      os.execute(command)
+      app.msgbox("Export finished!", {[1] = "Continue"})
+    end
+
+  end
+end
+
 function run()
 
-  -- Read settings: I use this (https://stackoverflow.com/a/41176958). An
-  -- alternative could have been https://stackoverflow.com/a/41176826. Both
-  -- found using G"lua read settings file".
-  local config = require "config"
-
-  local result = app.msgbox("Exports starts now, please wait until finished", {[1] = "Continue", [2] = "Cancel"})
-  if result == 1 then
-    command = config.python_executable .. " " .. config.xournalpp_htr_path
-              .. " -if " .. config.filename
-              .. "  -m " .. config.model
-              .. " -of " .. config.output_file
-    os.execute(command)
-    app.msgbox("Export finished!", {[1] = "Continue"})
-  end
+  app.fileDialogSave("save_file", "untitled.pdf")
 
   return
 end
