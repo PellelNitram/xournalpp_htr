ae3533f36f78145cea876b2809d52aac6cb3e00e
Martin L <konten.ma.le@gmail.com>
2024-06-29 16:59:51 +0100

    Update filename in `save_file` plugin function with document.

--- plugin/main.lua
+++ plugin/main.lua
@@ -10,6 +10,7 @@ function save_file(path)
     -- found using G"lua read settings file".
     local config = require "config"
 
+    config.filename = '"' .. app.getDocumentStructure()['xoppFilename'] .. '"'
     config.output_file = '"' .. path .. '"'
 
     command = config.python_executable .. " " .. config.xournalpp_htr_path
