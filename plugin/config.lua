717a6b3b4fe065e89bf776675c7e443e9eea6367
Martin L <konten.ma.le@gmail.com>
2024-06-29 16:44:05 +0100

    Add debug switch to plugin config.

--- plugin/config.lua
+++ plugin/config.lua
@@ -5,6 +5,7 @@ _M.python_executable = "/home/martin/anaconda3/envs/xournalpp_htr/bin/python"
 _M.xournalpp_htr_path = "/home/martin/Development/xournalpp_htr/xournalpp_htr/xournalpp_htr.py"
 _M.model = "dummy"
 _M.output_file = "/home/martin/Development/xournalpp_htr/tests/test_1_from_Xpp.pdf"
+_M.debug_HTR_command = false
 -- TODO: allow UI to set other parameters as well of `xournalpp_htr`.
 
 -- TODO replace later w/ temp exported file - filename will be derived automatically
