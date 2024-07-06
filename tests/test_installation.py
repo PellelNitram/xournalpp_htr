bacf8835ec88003d99b7cebef92b67043d5c48ce
Martin L <konten.ma.le@gmail.com>
2024-07-06 09:27:01 +0100

    Add test_xournalpp_htr_package_installation test.

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -1,3 +1,9 @@
 def test_htr_pipeline_package_installation():
     """Test if code from `htr_pipeline` package can be imported."""
     from htr_pipeline import read_page, DetectorConfig, LineClusteringConfig
+
+def test_xournalpp_htr_package_installation():
+    """Test if code from `xournalpp_htr` package can be imported."""
+    from xournalpp_htr.documents import XournalDocument
+    from xournalpp_htr.documents import XournalppDocument
+    from xournalpp_htr.utils import export_to_pdf_with_xournalpp
