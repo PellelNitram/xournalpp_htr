8a2c850c0cd8aa8ea46424511573a622220d3483
Martin L <konten.ma.le@gmail.com>
2024-07-06 09:25:58 +0100

    Add test_htr_pipeline_package_installation test.

---
+++ tests/test_installation.py
@@ -0,0 +1,3 @@
+def test_htr_pipeline_package_installation():
+    """Test if code from `htr_pipeline` package can be imported."""
+    from htr_pipeline import read_page, DetectorConfig, LineClusteringConfig
