c680d26739e756ba6fe882a2a147bdcb940a7265
Martin L <konten.ma.le@gmail.com>
2024-07-06 09:32:17 +0100

    Use installation pytest marker for test_installation tests.

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -1,7 +1,12 @@
+import pytest
+
+
+@pytest.mark.installation
 def test_htr_pipeline_package_installation():
     """Test if code from `htr_pipeline` package can be imported."""
     from htr_pipeline import read_page, DetectorConfig, LineClusteringConfig
 
+@pytest.mark.installation
 def test_xournalpp_htr_package_installation():
     """Test if code from `xournalpp_htr` package can be imported."""
     from xournalpp_htr.documents import XournalDocument
