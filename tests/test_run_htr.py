05ea7bab4e4d386cdfcf91abe5a44ad66a4be077
Martin L <konten.ma.le@gmail.com>
2024-07-25 10:45:17 +0100

    Add `test_parse_arguments_full` test.

--- tests/test_run_htr.py
+++ tests/test_run_htr.py
@@ -9,3 +9,12 @@ from xournalpp_htr.run_htr import main
 def test_parse_arguments_empty():
     with pytest.raises(SystemExit) as e_info:
         parse_arguments()
+
+def test_parse_arguments_full():
+    args = parse_arguments('-if input -of output -m dummy -pid dir -sp')
+    assert len(args) == 5
+    assert args['input_file'].stem == 'input'
+    assert args['output_file'].stem == 'output'
+    assert args['model'] == 'dummy'
+    assert args['prediction_image_dir'].stem == 'dir'
+    assert args['show_predictions'] == True
