625fcb7d0bd13cb00b91a4e0d437210c93b0f876
Martin L <konten.ma.le@gmail.com>
2024-07-25 09:31:22 +0100

    Add `input_file` guard to `export_to_pdf_with_xournalpp` function.

--- xournalpp_htr/utils.py
+++ xournalpp_htr/utils.py
@@ -33,6 +33,9 @@ def export_to_pdf_with_xournalpp(input_file: Path, output_file: Path) -> None:
             print(f"Export failed: {e}")
     """
 
+    if not input_file.exists():
+        raise ValueError(f'input file "{input_file}" does not exist.')
+
     command = f'xournalpp {input_file} -p {output_file}'
     export_result = subprocess.run(command,
                                    shell=True,
