d847476795d4c9a8cf5e58ec18dc9b7819334feb
Martin L <konten.ma.le@gmail.com>
2024-07-26 19:38:52 +0100

    Add `installation` pytest marker to `test_parse_arguments_empty`.

--- tests/test_run_htr.py
+++ tests/test_run_htr.py
@@ -6,6 +6,7 @@ from xournalpp_htr.run_htr import parse_arguments
 from xournalpp_htr.run_htr import main
 
 
+@pytest.mark.installation
 def test_parse_arguments_empty():
     with pytest.raises(SystemExit) as e_info:
         parse_arguments()
