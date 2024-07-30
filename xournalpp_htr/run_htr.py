215371ac0b5684fc09e8c83692b867ee50692c53
Martin L <konten.ma.le@gmail.com>
2024-07-30 10:43:57 +0100

    Apply git pre-commit hooks to `run_htr` module.

--- xournalpp_htr/run_htr.py
+++ xournalpp_htr/run_htr.py
@@ -4,34 +4,60 @@ import argparse
 from pathlib import Path
 
 from xournalpp_htr.documents import get_document
+from xournalpp_htr.models import compute_predictions, store_predictions_as_images
 from xournalpp_htr.utils import export_to_pdf_with_xournalpp
-from xournalpp_htr.xio import get_temporary_filename
-from xournalpp_htr.xio import write_predictions_to_PDF
-from xournalpp_htr.models import compute_predictions
-from xournalpp_htr.models import store_predictions_as_images
+from xournalpp_htr.xio import get_temporary_filename, write_predictions_to_PDF
 
 
 def parse_arguments(cli_string: None | str = None):
     """Parse arguments from command line."""
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-if', '--input-file', type=lambda p: Path(p).absolute(), required=True,
-                        help='Path to the input Xournal or Xournal++ file.')
-    parser.add_argument('-of', '--output-file', type=lambda p: Path(p).absolute(), required=True,
-                        help='Path to the output PDF file.')
+    parser = argparse.ArgumentParser(
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "-if",
+        "--input-file",
+        type=lambda p: Path(p).absolute(),
+        required=True,
+        help="Path to the input Xournal or Xournal++ file.",
+    )
+    parser.add_argument(
+        "-of",
+        "--output-file",
+        type=lambda p: Path(p).absolute(),
+        required=True,
+        help="Path to the output PDF file.",
+    )
     # v-- TODO: Make optional
-    parser.add_argument('-m', '--model', type=str, required=False, default='2024-07-18_htr_pipeline',
-                        help='The model to use for handwriting recognition.') # TODO: Introduce dummy model called "test_lua_to_python"; TODO: Register models somehow to allow choice keyword here; also add "none"; default to latest model
-    parser.add_argument('-pid', '--prediction-image-dir', type=lambda p: Path(p).absolute(), required=False,
-                        help='If provided, images of the pages with overlaid '
-                        'predictions are stored in the provided folder. '
-                        'Useful for debugging purposes.')
-    parser.add_argument('-sp', '--show-predictions', action='store_true',
-                        help='Store the predictions and bounding boxes '
-                        'visibly in the output file if enabled. '
-                        'Useful for debugging purposes. '
-                        'Otherwise only store invisible text.')
-    args = vars( parser.parse_args(cli_string.split() if cli_string else None) )
+    parser.add_argument(
+        "-m",
+        "--model",
+        type=str,
+        required=False,
+        default="2024-07-18_htr_pipeline",
+        help="The model to use for handwriting recognition.",
+    )  # TODO: Introduce dummy model called "test_lua_to_python"; TODO: Register models somehow to allow choice keyword here; also add "none"; default to latest model
+    parser.add_argument(
+        "-pid",
+        "--prediction-image-dir",
+        type=lambda p: Path(p).absolute(),
+        required=False,
+        help="If provided, images of the pages with overlaid "
+        "predictions are stored in the provided folder. "
+        "Useful for debugging purposes.",
+    )
+    parser.add_argument(
+        "-sp",
+        "--show-predictions",
+        action="store_true",
+        help="Store the predictions and bounding boxes "
+        "visibly in the output file if enabled. "
+        "Useful for debugging purposes. "
+        "Otherwise only store invisible text.",
+    )
+    args = vars(parser.parse_args(cli_string.split() if cli_string else None))
     return args
+
 
 def main(args: dict) -> None:
     """Main function that performs HTR.
@@ -41,7 +67,7 @@ def main(args: dict) -> None:
     the predictions to ensure they are working correctly and save them to a directory for later inspection.
 
     This function can be imported elsewhere to use, e.g., in Jupyter notebooks or tests.
-    
+
     :param args: Dictionary containing the following input parameters: `input_file' (Path; path to the input Xournal(++) file),
                  'prediction_image_dir' (Path or None; directory for storing prediction images (optional)), 'output_file'
                  (Path; path to the output PDF file), 'model' (str; name of the HTR model to use for predictions) and
@@ -55,11 +81,11 @@ def main(args: dict) -> None:
 
     # Settings
 
-    input_file = args['input_file']
-    prediction_image_dir = args['prediction_image_dir']
-    output_file = args['output_file']
-    debug_htr = args['show_predictions']
-    model = args['model']
+    input_file = args["input_file"]
+    prediction_image_dir = args["prediction_image_dir"]
+    output_file = args["output_file"]
+    debug_htr = args["show_predictions"]
+    model = args["model"]
 
     output_file_tmp_noOCR = get_temporary_filename()
 
@@ -78,7 +104,6 @@ def main(args: dict) -> None:
     # Plot the predictions to ensure that they are working correctly:
 
     if prediction_image_dir:
-
         store_predictions_as_images(prediction_image_dir, predictions, document)
 
     # Step 3: Store predictions in PDF
@@ -89,13 +114,13 @@ def main(args: dict) -> None:
         debug_htr,
     )
 
-    print('xournalpp_htr: Done!')
+    print("xournalpp_htr: Done!")
 
     # Step 4: Next steps
     #
     # I want to build prediction code that can run both in a CLI and in a notebook like this here. Also, I'd like to be able to set the model flexibly.
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = parse_arguments()
     main(args)
