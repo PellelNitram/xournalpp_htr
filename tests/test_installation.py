7f356c9fbad8b23984a5d9836a5a434f8d5f7419
Martin L <konten.ma.le@gmail.com>
2024-07-31 21:40:13 +0100

    Apply git pre-commit hooks to `test_installation` tests.

--- tests/test_installation.py
+++ tests/test_installation.py
@@ -1,14 +1,16 @@
+# ruff: noqa: F401
+
 import pytest
 
 
 @pytest.mark.installation
 def test_htr_pipeline_package_installation():
     """Test if code from `htr_pipeline` package can be imported."""
-    from htr_pipeline import read_page, DetectorConfig, LineClusteringConfig
+    from htr_pipeline import DetectorConfig, LineClusteringConfig, read_page
+
 
 @pytest.mark.installation
 def test_xournalpp_htr_package_installation():
     """Test if code from `xournalpp_htr` package can be imported."""
-    from xournalpp_htr.documents import XournalDocument
-    from xournalpp_htr.documents import XournalppDocument
+    from xournalpp_htr.documents import XournalDocument, XournalppDocument
     from xournalpp_htr.utils import export_to_pdf_with_xournalpp
