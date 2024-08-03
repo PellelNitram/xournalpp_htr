c4394691400b0b061338a53cb878db6d5f7c88e2
Martin L <konten.ma.le@gmail.com>
2024-08-03 13:46:24 +0100

    Improve docstring of `test_run_htr.get_repo_root_directory`.

--- tests/test_run_htr.py
+++ tests/test_run_htr.py
@@ -8,9 +8,19 @@ from xournalpp_htr.run_htr import main, parse_arguments
 
 @pytest.fixture
 def get_repo_root_directory(request: pytest.FixtureRequest) -> Path:
-    """TODO.
+    """
+    A `pytest` fixture that returns the root directory of the repository.
 
-    Fixture based on [this](https://stackoverflow.com/a/57039134).
+    This fixture retrieves the root directory from the pytest configuration
+    and asserts that the directory contains a "README.md" file. It is based
+    on an example provided in a
+    [Stack Overflow answer](https://stackoverflow.com/a/57039134).
+
+    :param request: A `pytest` fixture that provides information about the
+                    requesting test function.
+    :returns: A Path object representing the root directory of the repository.
+    :raises AssertionError: If the "README.md" file is not found in the root
+                            directory.
     """
     rootdir = Path(request.config.rootdir)
     assert (rootdir / "README.md").is_file()
