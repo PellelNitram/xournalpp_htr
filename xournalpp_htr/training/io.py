3cebc7d47652f814afc1dfbbce42e426f6373f77
Martin L <konten.ma.le@gmail.com>
2024-08-09 17:21:15 +0100

    Add `io.load_alphabet` function from PellelNitram/OnlineHTR repository.

--- xournalpp_htr/training/io.py
+++ xournalpp_htr/training/io.py
@@ -104,3 +104,14 @@ def store_alphabet(outfile: Path, alphabet: list[str]) -> None:
     """
     with open(outfile, "w") as f:
         json.dump({"alphabet": alphabet}, f, indent=4)
+
+
+def load_alphabet(infile: Path) -> list[str]:
+    """Load alphabet from JSON.
+
+    :param infile: The path to load the alphabet from.
+    :returns: The alphabet as list of strings.
+    """
+    with open(infile, "r") as f:
+        json_data = json.load(f)
+    return json_data["alphabet"]
