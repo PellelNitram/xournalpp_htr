5fe4112288da3c399ac8c37ee9dd15386bd54c30
Martin L <konten.ma.le@gmail.com>
2024-08-09 18:44:49 +0100

    Add `test_get_document_xopp` test.

--- tests/test_XournalDocument.py
+++ tests/test_XournalDocument.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from xournalpp_htr.documents import XournalppDocument
+from xournalpp_htr.documents import XournalppDocument, get_document
 
 
 def test_XournalppDocument(get_path_to_minimal_test_data: Path):
@@ -18,3 +18,20 @@ def test_XournalppDocument(get_path_to_minimal_test_data: Path):
     assert len(page.layers) == 1
     layer = page.layers[0]
     assert len(layer.strokes) == 85
+
+
+def test_get_document_xopp(get_path_to_minimal_test_data: Path):
+    """Tests `get_document` function for a `xopp` file."""
+    xpp_document = XournalppDocument(get_path_to_minimal_test_data)
+    document = get_document(get_path_to_minimal_test_data)
+    assert xpp_document.path == document.path == get_path_to_minimal_test_data
+    assert len(xpp_document.pages) == len(document.pages) == 1
+    assert xpp_document.DPI == document.DPI
+    assert xpp_document.pages[0].meta_data == document.pages[0].meta_data
+    assert xpp_document.pages[0].background == document.pages[0].background
+    assert len(xpp_document.pages[0].layers) == len(document.pages[0].layers) == 1
+    assert (
+        len(xpp_document.pages[0].layers[0].strokes)
+        == len(document.pages[0].layers[0].strokes)
+        == 85
+    )
