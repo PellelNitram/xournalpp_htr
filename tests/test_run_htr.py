86e53e2e58343ece2926175b64feb75889382a25
Martin L <konten.ma.le@gmail.com>
2024-08-09 17:48:46 +0100

    Move `pytest` fixtures in `test_run_htr` to central location.

--- tests/test_run_htr.py
+++ tests/test_run_htr.py
@@ -1,56 +1,8 @@
-import urllib.request
 from pathlib import Path
 
 import pytest
 
 from xournalpp_htr.run_htr import main, parse_arguments
-
-
-@pytest.fixture
-def get_repo_root_directory(request: pytest.FixtureRequest) -> Path:
-    """
-    A `pytest` fixture that returns the root directory of the repository.
-
-    This fixture retrieves the root directory from the pytest configuration
-    and asserts that the directory contains a "README.md" file. It is based
-    on an example provided in a
-    [Stack Overflow answer](https://stackoverflow.com/a/57039134).
-
-    :param request: A `pytest` fixture that provides information about the
-                    requesting test function.
-    :returns: A Path object representing the root directory of the repository.
-    :raises AssertionError: If the "README.md" file is not found in the root
-                            directory.
-    """
-    rootdir = Path(request.config.rootdir)
-    assert (rootdir / "README.md").is_file()
-    return rootdir
-
-
-@pytest.fixture
-def get_path_to_minimal_test_data(get_repo_root_directory: Path) -> Path:
-    """Fixture to retrieve the path to the minimal test data file.
-
-    This function checks for the existence of a specific test data file
-    located at `${repo_root}/tests/data/2024-07-26_minimal.xopp`. If the
-    file does not exist, it is downloaded from a predefined URL and saved
-    at the specified location so that it can be retrieved directly next
-    time; i.e. it is cached.
-
-    :param get_repo_root_directory: A fixture that provides the root
-                                    directory of the repository.
-    :returns: The path to the minimal test data file.
-    """
-
-    path_to_minimal_test_data = (
-        get_repo_root_directory / "tests/data/2024-07-26_minimal.xopp"
-    )
-
-    if not path_to_minimal_test_data.is_file():
-        url = "https://bit.ly/2024-07-26_minimal_xopp"
-        urllib.request.urlretrieve(url, path_to_minimal_test_data)
-
-    return path_to_minimal_test_data
 
 
 @pytest.mark.installation
