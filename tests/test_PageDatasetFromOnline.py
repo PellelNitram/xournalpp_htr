05a14506a8d319973e4929df77189ee0dd6c7258
Martin L <konten.ma.le@gmail.com>
2024-08-16 12:23:02 +0100

    Add reminder errors to `test_PageDatasetFromOnline` tests.

--- tests/test_PageDatasetFromOnline.py
+++ tests/test_PageDatasetFromOnline.py
@@ -1,6 +1,9 @@
 def test_compute():
+    raise NotImplementedError("TODO")
     pass
 
+
 def test_render_pages():
+    raise NotImplementedError("TODO")
     pass
     # TODO: Make sure to check the pages visually
