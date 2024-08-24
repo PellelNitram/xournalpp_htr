b05319483f1374faa96589d678d1f3013b35e756
Martin L <konten.ma.le@gmail.com>
2024-08-24 15:40:24 +0100

    Start to work on `test_PageDatasetFromOnline.test_getitem` test.

--- tests/test_PageDatasetFromOnline.py
+++ tests/test_PageDatasetFromOnline.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import pytest
+import torch
 
 from xournalpp_htr.training.data.datasets import (
     IAM_OnDB_Dataset,
@@ -103,3 +104,55 @@ def test_render_pages(get_path_to_IAM_OnDB_dataset: Path, tmp_path: Path):
     )
     for key in p_ds.data:
         p_ds.render_pages(key, tmp_path / f"test_{key}.png")
+
+
+@pytest.mark.visual_check
+def test_getitem(get_path_to_IAM_OnDB_dataset: Path, tmp_path: Path):
+    print(f"\n\nPath to check out: {tmp_path}.\n")
+
+    limit = 100
+    ds = IAM_OnDB_Dataset(
+        path=get_path_to_IAM_OnDB_dataset, transform=None, limit=limit
+    )
+
+    positions_height = 5.0
+    positions = [
+        PageDatasetFromOnlinePosition(
+            stroke_width=1,
+            page_index=0,
+            center_x=10.0,
+            center_y=10.0,
+            height=positions_height,
+            dataset_index=0,
+        ),
+        PageDatasetFromOnlinePosition(
+            stroke_width=1,
+            page_index=0,
+            center_x=10.0,
+            center_y=20.0,
+            height=positions_height,
+            dataset_index=1,
+        ),
+    ]
+
+    p_ds = PageDatasetFromOnline(
+        dataset=ds,
+        positions=positions,
+        page_size=(10, 10),
+        cache_dir=tmp_path,
+        dpi=72,
+    )
+    sample = p_ds[0]
+
+    image = sample["image"]
+    # segmentation_mask = sample["segmentation_mask"]
+
+    assert isinstance(image, torch.Tensor)
+    assert image.shape[0] == 4  # This might change!
+    assert image.dtype == torch.uint8
+
+    # TODO: Check segmentation_mask, e.g.:
+    # - image.shape[1:] == segmentation_mask[1:]
+
+    # for key in p_ds.data:
+    #     p_ds.render_pages(key, tmp_path / f"test_{key}.png")
