d6681fd53344c66b80bff65e3ffc5355e3c907e1
hfbauman <60202942+hfbauman@users.noreply.github.com>
2024-09-10 21:21:52 -0400

    Modify setup to find and complete paths in config.lua

--- setup.py
+++ setup.py
@@ -1,4 +1,40 @@
+import os
+import sys
+
 import setuptools
+
+## Modifies config.lua to use the appropriate paths
+# Get the path of this file
+htr_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Path to the config.lua file
+config_file = os.path.join(htr_dir, "plugin", "config.lua")
+
+# Fix direction of slashes, needed on Windows
+htr_dir = htr_dir.replace("\\", "/")
+
+# Get the path of the Python executable
+python_executable = sys.executable.replace("\\", "/")
+
+# Modify the config.lua file
+with open(config_file, "r") as f:
+    lines = f.readlines()
+
+# Modify the necessary lines in the config.lua file
+modified_lines = []
+for line in lines:
+    if line.startswith("_M.python_executable ="):
+        modified_lines.append('_M.python_executable = "' + python_executable + '"\n')
+    elif line.startswith("_M.xournalpp_htr_path ="):
+        modified_lines.append(
+            '_M.xournalpp_htr_path = "' + htr_dir + '/xournalpp_htr/run_htr.py"\n'
+        )
+    else:
+        modified_lines.append(line)
+
+# Write the modified lines back to the config.lua file
+with open(config_file, "w") as f:
+    f.writelines(modified_lines)
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
