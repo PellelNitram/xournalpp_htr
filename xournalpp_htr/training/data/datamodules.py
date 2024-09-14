5e85b84d9330b6c82cf37c3d998a1790abdbfd43
Martin L <konten.ma.le@gmail.com>
2024-09-14 21:30:39 +0100

    Add empty `WordDetectorNN` class.

--- xournalpp_htr/training/data/datamodules.py
+++ xournalpp_htr/training/data/datamodules.py
@@ -1,1 +1,11 @@
+from lightning import LightningModule
 
+
+class WordDetectorNN(LightningModule):
+    """
+    My implementation of [WordDetectorNN](https://github.com/githubharald/WordDetectorNN/)
+    by [Harald Scheidl](Harald Scheidl).
+    """
+
+    def __init__(self):
+        pass
