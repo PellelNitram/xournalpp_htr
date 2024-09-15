4e6d99bb6b09ff182d72d49ea5d038eaaded8dea
Martin L <konten.ma.le@gmail.com>
2024-09-15 07:52:30 +0100

    Move `WordDetectorNN` from `datamodules` to `models`.

--- xournalpp_htr/training/models.py
+++ xournalpp_htr/training/models.py
@@ -1,1 +1,13 @@
 """Module concerned with models for training custom xournalpp_htr models."""
+
+from lightning import LightningModule
+
+
+class WordDetectorNN(LightningModule):
+    """
+    My implementation of [WordDetectorNN](https://github.com/githubharald/WordDetectorNN/)
+    by [Harald Scheidl](Harald Scheidl).
+    """
+
+    def __init__(self):
+        pass
