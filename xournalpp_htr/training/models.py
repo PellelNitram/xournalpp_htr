30d17c23c331db97cf29a19599c79595523e7ccd
Martin L <konten.ma.le@gmail.com>
2024-09-18 12:10:45 +0100

    Work on WordDetectorNN class.

--- xournalpp_htr/training/models.py
+++ xournalpp_htr/training/models.py
@@ -7,7 +7,13 @@ class WordDetectorNN(LightningModule):
     """
     My implementation of [WordDetectorNN](https://github.com/githubharald/WordDetectorNN/)
     by [Harald Scheidl](Harald Scheidl).
+
+    See [here](https://lightning.ai/docs/pytorch/stable/common/lightning_module.html) for information
+    on PyTorch Lightning's `LightningModule`.
     """
 
     def __init__(self):
+        pass
+
+    def forward(self, inputs):
         pass
