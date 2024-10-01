f197d4d4404411cb59d60376cb91d28557e58a74
Martin L <konten.ma.le@gmail.com>
2024-10-01 12:59:26 +0100

    Work on Python annotate tool.

--- xournalpp_htr/training/annotate.py
+++ xournalpp_htr/training/annotate.py
@@ -66,12 +66,12 @@ This script helps to annotate Xournal(++) files.
 
 # I use this to speed up the process
 
-# TODO: define storage schema to allow backward compatibility when improving the script later on
+# TODO: Properly read about TKINTER interface design
+# - https://tkinterpython.top/layout/
 
 # TODO: Adhere to design document called `annotate_tool_UI_design.svg`. Potentially model it in PenPot?
 
-# TODO: Properly read about TKINTER interface design
-# - https://tkinterpython.top/layout/
+# TODO: define storage schema to allow backward compatibility when improving the script later on
 
 # -*- coding: utf-8 -*-
 # Advanced zoom example. Like in Google Maps.
@@ -80,6 +80,7 @@ This script helps to annotate Xournal(++) files.
 import random
 import tkinter as tk
 from tkinter import ttk
+from tkinter.filedialog import askopenfilename
 
 from PIL import Image, ImageTk
 
@@ -268,43 +269,61 @@ class Zoom_Advanced(ttk.Frame):
 
 root = tk.Tk()  # create root window
 root.title("Annotate Tool")  # title of the GUI window
-root.maxsize(900, 600)  # specify the max size the window can expand to
+# root.maxsize(900, 600)  # specify the max size the window can expand to
+root.geometry("400x400")
 root.config(bg="skyblue")  # specify background color
 
-# Create left and right frames
-left_frame = tk.Frame(root, width=200, height=400, bg="grey")
-left_frame.grid(row=0, column=0, padx=10, pady=5)
 
-right_frame = tk.Frame(root, width=650, height=400, bg="grey")
-right_frame.grid(row=0, column=1, padx=10, pady=5)
+currently_loaded_document = None
 
-# Create frames and labels in left_frame
-tk.Label(left_frame, text="Original Image").grid(row=0, column=0, padx=5, pady=5)
 
-# load image to be "edited"
-image = tk.PhotoImage(file="/home/martin/Downloads/small_VC.gif")
-original_image = image.subsample(3, 3)  # resize image using subsample
-tk.Label(left_frame, image=original_image).grid(row=1, column=0, padx=5, pady=5)
+def load_document():
+    global currently_loaded_document
+    filename = askopenfilename()
+    print("before:", currently_loaded_document)
+    currently_loaded_document = filename
+    print("after:", currently_loaded_document)
+    return filename
 
-# Display image in right_frame
-tk.Label(right_frame, image=image).grid(row=0, column=0, padx=5, pady=5)
 
-# Create tool bar frame
-tool_bar = tk.Frame(left_frame, width=180, height=185)
-tool_bar.grid(row=2, column=0, padx=5, pady=5)
+w = tk.Button(root, text="Load document", command=load_document)
+w.place(x=50, y=50)
 
-# Example labels that serve as placeholders for other widgets
-tk.Label(tool_bar, text="Tools", relief=tk.RAISED).grid(
-    row=0, column=0, padx=5, pady=3, ipadx=10
-)  # ipadx is padding inside the Label widget
-tk.Label(tool_bar, text="Filters", relief=tk.RAISED).grid(
-    row=0, column=1, padx=5, pady=3, ipadx=10
-)
+# # Create left and right frames
+# left_frame = tk.Frame(root, width=200, height=400, bg="grey")
+# left_frame.grid(row=0, column=0, padx=10, pady=5)
 
-# Example labels that could be displayed under the "Tool" menu
-tk.Label(tool_bar, text="Select").grid(row=1, column=0, padx=5, pady=5)
-tk.Label(tool_bar, text="Crop").grid(row=2, column=0, padx=5, pady=5)
-tk.Label(tool_bar, text="Rotate & Flip").grid(row=3, column=0, padx=5, pady=5)
-tk.Label(tool_bar, text="Resize").grid(row=4, column=0, padx=5, pady=5)
-tk.Label(tool_bar, text="Exposure").grid(row=5, column=0, padx=5, pady=5)
+# right_frame = tk.Frame(root, width=650, height=400, bg="grey")
+# right_frame.grid(row=0, column=1, padx=10, pady=5)
+
+# # Create frames and labels in left_frame
+# tk.Label(left_frame, text="Original Image").grid(row=0, column=0, padx=5, pady=5)
+
+# # load image to be "edited"
+# image = tk.PhotoImage(file="/home/martin/Downloads/small_VC.gif")
+# original_image = image.subsample(3, 3)  # resize image using subsample
+# tk.Label(left_frame, image=original_image).grid(row=1, column=0, padx=5, pady=5)
+
+# # Display image in right_frame
+# tk.Label(right_frame, image=image).grid(row=0, column=0, padx=5, pady=5)
+
+# # Create tool bar frame
+# tool_bar = tk.Frame(left_frame, width=180, height=185)
+# tool_bar.grid(row=2, column=0, padx=5, pady=5)
+
+# # Example labels that serve as placeholders for other widgets
+# tk.Label(tool_bar, text="Tools", relief=tk.RAISED).grid(
+#     row=0, column=0, padx=5, pady=3, ipadx=10
+# )  # ipadx is padding inside the Label widget
+# tk.Label(tool_bar, text="Filters", relief=tk.RAISED).grid(
+#     row=0, column=1, padx=5, pady=3, ipadx=10
+# )
+
+# # Example labels that could be displayed under the "Tool" menu
+# tk.Label(tool_bar, text="Select").grid(row=1, column=0, padx=5, pady=5)
+# tk.Label(tool_bar, text="Crop").grid(row=2, column=0, padx=5, pady=5)
+# tk.Label(tool_bar, text="Rotate & Flip").grid(row=3, column=0, padx=5, pady=5)
+# tk.Label(tool_bar, text="Resize").grid(row=4, column=0, padx=5, pady=5)
+# tk.Label(tool_bar, text="Exposure").grid(row=5, column=0, padx=5, pady=5)
+
 root.mainloop()
