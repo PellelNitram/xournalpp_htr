8217bb294db3c04ab562f3e44560500b11cd455f
Martin L <konten.ma.le@gmail.com>
2024-10-13 09:32:08 +0100

    Tidy up `annotate.py`.
    Remove code that was commented out and related to component layout.

--- xournalpp_htr/training/annotate.py
+++ xournalpp_htr/training/annotate.py
@@ -591,41 +591,4 @@ export_annotations.place(x=50, y=750)
 
 # TODO: check why there's an exception (perceived somewhat randomly)
 
-# # Create left and right frames
-# left_frame = tk.Frame(root, width=200, height=400, bg="grey")
-# left_frame.grid(row=0, column=0, padx=10, pady=5)
-
-# right_frame = tk.Frame(root, width=650, height=400, bg="grey")
-# right_frame.grid(row=0, column=1, padx=10, pady=5)
-
-# # Create frames and labels in left_frame
-# tk.Label(left_frame, text="Original Image").grid(row=0, column=0, padx=5, pady=5)
-
-# # load image to be "edited"
-# image = tk.PhotoImage(file="/home/martin/Downloads/small_VC.gif")
-# original_image = image.subsample(3, 3)  # resize image using subsample
-# tk.Label(left_frame, image=original_image).grid(row=1, column=0, padx=5, pady=5)
-
-# # Display image in right_frame
-# tk.Label(right_frame, image=image).grid(row=0, column=0, padx=5, pady=5)
-
-# # Create tool bar frame
-# tool_bar = tk.Frame(left_frame, width=180, height=185)
-# tool_bar.grid(row=2, column=0, padx=5, pady=5)
-
-# # Example labels that serve as placeholders for other widgets
-# tk.Label(tool_bar, text="Tools", relief=tk.RAISED).grid(
-#     row=0, column=0, padx=5, pady=3, ipadx=10
-# )  # ipadx is padding inside the Label widget
-# tk.Label(tool_bar, text="Filters", relief=tk.RAISED).grid(
-#     row=0, column=1, padx=5, pady=3, ipadx=10
-# )
-
-# # Example labels that could be displayed under the "Tool" menu
-# tk.Label(tool_bar, text="Select").grid(row=1, column=0, padx=5, pady=5)
-# tk.Label(tool_bar, text="Crop").grid(row=2, column=0, padx=5, pady=5)
-# tk.Label(tool_bar, text="Rotate & Flip").grid(row=3, column=0, padx=5, pady=5)
-# tk.Label(tool_bar, text="Resize").grid(row=4, column=0, padx=5, pady=5)
-# tk.Label(tool_bar, text="Exposure").grid(row=5, column=0, padx=5, pady=5)
-
 root.mainloop()
