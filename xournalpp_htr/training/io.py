ad8b9a8c7c9c73cefe4ef7aa6ed810bb40cc5797
Martin L <konten.ma.le@gmail.com>
2024-10-14 13:56:32 +0100

    Improve `store_list_of_bboxes`.
    - Add `schema_version` check.
    - Fix inefficiency bug that overwrote meta data in `storage` in bbox
      loop.

--- xournalpp_htr/training/io.py
+++ xournalpp_htr/training/io.py
@@ -71,34 +71,42 @@ def store_list_of_bboxes(
     TODO: Add schema to version data storage and loading properly. This is to make
     the anntation process future proof for upcoming `annotate.py` versions.
     """
-    storage = {"bboxes": []}
 
-    for bbox in list_of_bboxes:
-        value = {}
+    if schema_version == "v1_2024-10-13":
+        storage = {
+            "annotator_ID": meta_data["annotator_ID"],
+            "writer_ID": meta_data["writer_ID"],
+            "currently_loaded_document": meta_data["currently_loaded_document"],
+            "page_index": meta_data["page_index"],
+            "schema_version": "v1_2024-10-13",
+            "bboxes": [],
+        }
 
-        # TODO: Use BBox.as_json_str; how does that work w/ `strokes` list?
-        value["capture_date"] = str(bbox.capture_date)
-        value["point_1_x"] = bbox.point_1_x
-        value["point_1_y"] = bbox.point_1_y
-        value["point_2_x"] = bbox.point_2_x
-        value["point_2_y"] = bbox.point_2_y
-        value["text"] = bbox.text
-        value["uuid"] = bbox.uuid
-        value["bbox_strokes"] = []
-        for stroke in bbox.strokes:
-            value["bbox_strokes"].append(
-                {
-                    "meta_data": stroke.meta_data,
-                    "x": stroke.x.tolist(),
-                    "y": stroke.y.tolist(),
-                }
-            )
+        for bbox in list_of_bboxes:
+            value = {}
 
-        storage["bboxes"].append(value)
-        storage["annotator_ID"] = meta_data["annotator_ID"]
-        storage["writer_ID"] = meta_data["writer_ID"]
-        storage["currently_loaded_document"] = meta_data["currently_loaded_document"]
-        storage["page_index"] = meta_data["page_index"]
+            # TODO: Use BBox.as_json_str; how does that work w/ `strokes` list?
+            value["capture_date"] = str(bbox.capture_date)
+            value["point_1_x"] = bbox.point_1_x
+            value["point_1_y"] = bbox.point_1_y
+            value["point_2_x"] = bbox.point_2_x
+            value["point_2_y"] = bbox.point_2_y
+            value["text"] = bbox.text
+            value["uuid"] = bbox.uuid
+            value["bbox_strokes"] = []
+            for stroke in bbox.strokes:
+                value["bbox_strokes"].append(
+                    {
+                        "meta_data": stroke.meta_data,
+                        "x": stroke.x.tolist(),
+                        "y": stroke.y.tolist(),
+                    }
+                )
 
-    with open(output_path, mode="w") as f:
-        json.dump(storage, f)
+            storage["bboxes"].append(value)
+
+        with open(output_path, mode="w") as f:
+            json.dump(storage, f)
+
+    else:
+        raise ValueError(f'"schema_version"={schema_version} not implemented')
