9c763283d226b953662b70f0e7c9cd02e2b53fd9
Martin Lellep <konten.ma.le@gmail.com>
2025-08-16 06:18:46 +0000

    Move loss function from notebook to code library.

--- xournalpp_htr/training/WordDetectorNN/my_code.py
+++ xournalpp_htr/training/WordDetectorNN/my_code.py
@@ -654,3 +654,41 @@ def draw_bboxes_on_image(
         )
 
     return img
+
+def compute_loss(y, gt_map):
+
+    # TODO: refactoring possibility: compute area, can be used 2 to 3 times
+    # TODO: add weights in loss and hyperparameter-tune them
+
+    # 1. segmentation loss
+    target_labels = torch.argmax(gt_map[:, MapOrdering.SEG_WORD:MapOrdering.SEG_BACKGROUND + 1], dim=1)
+    loss_seg = F.cross_entropy(y[:, MapOrdering.SEG_WORD:MapOrdering.SEG_BACKGROUND + 1], target_labels)
+
+    # 2. geometry loss
+    # distances to all sides of aabb
+    t = torch.minimum(y[:, MapOrdering.GEO_TOP], gt_map[:, MapOrdering.GEO_TOP])
+    b = torch.minimum(y[:, MapOrdering.GEO_BOTTOM], gt_map[:, MapOrdering.GEO_BOTTOM])
+    l = torch.minimum(y[:, MapOrdering.GEO_LEFT], gt_map[:, MapOrdering.GEO_LEFT])
+    r = torch.minimum(y[:, MapOrdering.GEO_RIGHT], gt_map[:, MapOrdering.GEO_RIGHT])
+
+    # area of predicted aabb
+    y_width = y[:, MapOrdering.GEO_LEFT, ...] + y[:, MapOrdering.GEO_RIGHT, ...]
+    y_height = y[:, MapOrdering.GEO_TOP, ...] + y[:, MapOrdering.GEO_BOTTOM, ...]
+    area1 = y_width * y_height
+
+    # area of gt aabb
+    gt_width = gt_map[:, MapOrdering.GEO_LEFT, ...] + gt_map[:, MapOrdering.GEO_RIGHT, ...]
+    gt_height = gt_map[:, MapOrdering.GEO_TOP, ...] + gt_map[:, MapOrdering.GEO_BOTTOM, ...]
+    area2 = gt_width * gt_height
+
+    # compute intersection over union
+    intersection = (r + l) * (b + t)
+    union = area1 + area2 - intersection
+    eps = 0.01  # avoid division by 0
+    iou = intersection / (union + eps)
+    iou = iou[gt_map[:, MapOrdering.SEG_WORD] > 0]
+    loss_aabb = -torch.log(torch.mean(iou))
+
+    # total loss is simply the sum of both losses
+    loss = loss_seg + loss_aabb
+    return loss
