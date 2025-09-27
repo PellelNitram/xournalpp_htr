ff674efbb69d9d2bb2c6d06cbd905d693ddfc53e
Martin L <konten.ma.le@gmail.com>
2025-09-28 00:36:44 +0200

    Remove obsolete comment in `run_htr` script.

--- xournalpp_htr/run_htr.py
+++ xournalpp_htr/run_htr.py
@@ -3,11 +3,6 @@
 from xournalpp_htr.pipeline import export_xournalpp_to_pdf_with_htr
 from xournalpp_htr.utils import parse_arguments
 
-# Step 4: Next steps
-#
-# I want to build prediction code that can run both in a CLI and in a notebook like this here. Also, I'd like to be able to set the model flexibly.
-
-
 if __name__ == "__main__":
     args = parse_arguments()
     export_xournalpp_to_pdf_with_htr(args)
