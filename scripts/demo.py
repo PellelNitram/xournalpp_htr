010adf7eb795db133a3fa0d084f2235e55adf3f0
Martin L <konten.ma.le@gmail.com>
2025-10-15 21:24:17 +0200

    Remove old commented out code in Gradio demo.

--- scripts/demo.py
+++ scripts/demo.py
@@ -135,35 +135,3 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 7860))  # Use HF-provided port or fallback
     demo.launch(server_name="0.0.0.0", server_port=port)
-
-
-# import os
-
-# import gradio as gr
-
-# from xournalpp_htr.documents import Stroke
-
-# # TODO: Add gradio demo here.
-# # - it needs to run locally
-# # - it needs to run in a docker container that's compatible w/ huggingface space
-
-
-# def greet(name):
-#     s = Stroke  # Just for testing import
-#     print(s)
-#     return f"Hello, {name}!"
-
-
-# with gr.Blocks() as demo:
-#     gr.Markdown("# Greeting App")
-#     with gr.Row():
-#         name_input = gr.Textbox(
-#             label="Enter your name", placeholder="Type your name here..."
-#         )
-#         output = gr.Textbox(label="Greeting")
-#     greet_button = gr.Button("Greet")
-#     greet_button.click(fn=greet, inputs=name_input, outputs=output)
-
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 7860))  # Use HF-provided port or fallback
-#     demo.launch(server_name="0.0.0.0", server_port=port)
