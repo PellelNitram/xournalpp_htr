fdcfb59cd751f19c6d396365774d8e333102fea6
Martin L <konten.ma.le@gmail.com>
2025-11-26 21:30:51 +0100

    Update test script.

--- xournalpp_htr/fix_empty_file_crash_test.sh
+++ xournalpp_htr/fix_empty_file_crash_test.sh
@@ -1,5 +1,9 @@
+set -e
+
 SUFFIX=xopp
 
 INPUT_FILE=/home/martin/.cache/huggingface/hub/datasets--PellelNitram--xournalpp_htr_examples/snapshots/d46812cf696a52322aba3fd5cfbd45f8b3352343/data/empty.${SUFFIX}
+python run_htr.py -if ${INPUT_FILE} -of /tmp/test_empty.pdf
 
-python run_htr.py -if ${INPUT_FILE} -of test.pdf
+INPUT_FILE=/home/martin/.cache/huggingface/hub/datasets--PellelNitram--xournalpp_htr_examples/snapshots/d46812cf696a52322aba3fd5cfbd45f8b3352343/data/first_upload.xoj
+python run_htr.py -if ${INPUT_FILE} -of /tmp/test_non_empty.pdf
