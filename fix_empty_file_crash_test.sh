30d9e6a590337feaff3c286c717deb677a2ea85d
Martin L <konten.ma.le@gmail.com>
2025-11-26 20:48:40 +0100

    Update test script to use sample from HF dataset.

--- fix_empty_file_crash_test.sh
+++ fix_empty_file_crash_test.sh
@@ -1,1 +1,5 @@
-python run_htr.py -if ~/data/xournalpp_htr/empty_2025-11-15.xopp -of test.pdf
+SUFFIX=.xopp
+
+INPUT_FILE=/home/martin/.cache/huggingface/hub/datasets--PellelNitram--xournalpp_htr_examples/snapshots/d46812cf696a52322aba3fd5cfbd45f8b3352343/data/empty.${SUFFIX}
+
+python run_htr.py -if ${INPUT_FILE} -of test.pdf
