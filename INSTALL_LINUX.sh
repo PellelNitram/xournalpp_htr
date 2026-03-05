8ddb8a0870ab8e9e4e96c5276fd1e35d569c1f60
Martin L <konten.ma.le@gmail.com>
2026-03-05 13:18:55 +0100

    Migrate install scripts to uv and remove requirements files. [CC]

--- INSTALL_LINUX.sh
+++ INSTALL_LINUX.sh
@@ -2,7 +2,6 @@
 # SETTINGS
 # ========
 
-ENVIRONMENT_NAME="xournalpp_htr"
 HTR_PIPELINE_PATH="external/htr_pipeline"
 
 # ================
@@ -19,8 +18,6 @@ install_htr_pipeline () {
     wget https://www.dropbox.com/s/j1hl6bppecug0sz/models.zip
     unzip -o models.zip
     cd ../../
-    pip install .
-    # 3. Install [HTRPipelines](https://github.com/githubharald/HTRPipeline) package using [its installation guide](https://github.com/githubharald/HTRPipeline/tree/master#installation).
 
 }
 
@@ -32,16 +29,11 @@ CURRENT_DIR=$(pwd)
 
 rm -rf ${HTR_PIPELINE_PATH}
 
-eval "$(conda shell.bash hook)" # enable `conda activate`, see
-                                # https://stackoverflow.com/a/56155771
-
-conda create --name ${ENVIRONMENT_NAME} python=3.10.11 -y
-conda activate ${ENVIRONMENT_NAME}
 install_htr_pipeline
 cd ${CURRENT_DIR}
-pip install -r requirements.txt
-pip install -e .
-pre-commit install
+uv sync
+uv run xournalpp-htr-configure
+uv run pre-commit install
 
 cd plugin
 bash copy_to_plugin_folder.sh
@@ -57,6 +49,6 @@ echo "==========================================="
 echo
 echo "Installation complete"
 echo
-echo "Activate environment with:"
-echo "\"conda activate ${ENVIRONMENT_NAME}\""
+echo "Run commands in the environment with:"
+echo "\"uv run <command>\""
 echo
