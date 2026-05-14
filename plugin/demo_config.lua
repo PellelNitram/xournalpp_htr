0279c1f4dd754d654ca0b982e532849c0fa52a58
Martin L <konten.ma.le@gmail.com>
2026-05-14 14:32:43 +0200

    Replace local-only paths in plugin configs with placeholders. [CC]

--- plugin/demo_config.lua
+++ plugin/demo_config.lua
@@ -1,14 +1,14 @@
 local _M = {}
 
 -- user settings
-_M.python_executable = "/home/martin/anaconda3/envs/xournalpp_htr/bin/python"
-_M.xournalpp_htr_path = "/home/martin/Development/xournalpp_htr/xournalpp_htr/run_htr.py"
+_M.python_executable = "/path/to/python"
+_M.xournalpp_htr_path = "/path/to/xournalpp_htr/run_htr.py"
 _M.pipeline = "dummy"
-_M.output_file = "/home/martin/Development/xournalpp_htr/tests/test_1_from_Xpp.pdf"
+_M.output_file = "/path/to/output.pdf"
 _M.debug_HTR_command = false
 -- TODO: allow UI to set other parameters as well of `xournalpp_htr`.
 
 -- TODO replace later w/ temp exported file - filename will be derived automatically
-_M.filename = "/home/martin/Development/xournalpp_htr/tests/test_1.xoj"
+_M.filename = "/path/to/input.xoj"
 
 return _M
