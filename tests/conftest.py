10a8917c9879d801628c27a8056d5a46224cd3eb
Martin L <konten.ma.le@gmail.com>
2026-05-14 15:22:02 +0200

    Drop unused get_repo_root_directory test fixture. [CC]
    Not requested by any test. Issue #88.

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,27 +7,6 @@ from xournalpp_htr.xio import load_IAM_OnDB_dataset
 
 
 @pytest.fixture
-def get_repo_root_directory(request: pytest.FixtureRequest) -> Path:
-    """
-    A `pytest` fixture that returns the root directory of the repository.
-
-    This fixture retrieves the root directory from the pytest configuration
-    and asserts that the directory contains a "README.md" file. It is based
-    on an example provided in a
-    [Stack Overflow answer](https://stackoverflow.com/a/57039134).
-
-    :param request: A `pytest` fixture that provides information about the
-                    requesting test function.
-    :returns: A Path object representing the root directory of the repository.
-    :raises AssertionError: If the "README.md" file is not found in the root
-                            directory.
-    """
-    rootdir = Path(request.config.rootdir)
-    assert (rootdir / "README.md").is_file()
-    return rootdir
-
-
-@pytest.fixture
 def get_path_to_minimal_test_data() -> Path:
     """Fixture to retrieve the path to the minimal test data file.
 
